c48898f189a7ddd5d9d13a24e701edf263f6d3eb
Senhui Guo <general@sguo.org>
2021-06-02 11:34:28 +1000

    add additional links

--- assets/js/pub_gen.js
+++ assets/js/pub_gen.js
@@ -21,45 +21,107 @@ readTextFile('https://raw.githubusercontent.com/wlouyang/wlouyang.github.io/mast
   conference_data = data.conferences;
 });
 
-
 function journalPubItemConstructor(pubitem) {
+  function filter_mandatory_members(value, index, arr) {
+    let mandatory_members = ["authors", "title", "publisher", "image"];
+    return !mandatory_members.includes(value);
+  }
+  let all_members = Object.keys(pubitem);
+  all_members = all_members.filter(filter_mandatory_members);
   let obj;
-  obj = document.createElement('template');
+  obj = document.createElement("template");
+  let additional_link_template = `[<a class="aLink" href="LINK_TO_REPLACE" >TEXT_TO_REPLACE</a>]`;
   let html_template = `
-    <div>
-      <div class="row">
-        <div class="small-4 column">
-            <img src="IMAGE" style="width: 100%">
-        </div>
-        <div class="small-8 column">
-          <p>
-            <b>TITLE</b>
-            <br>
-            AUTHORS
-            <br> 
-            JOURNAL
-            <br>
-          </p>
-        </div>
-          
+  <div>
+    <div class="row">
+      <div class="small-4 column">
+          <img src="IMAGE" style="width: 100%">
       </div>
-      <hr>
+      <div class="small-8 column">
+        <p>
+          <b>TITLE</b>
+          <br>
+          AUTHORS
+          <br> 
+          JOURNAL
+          <br>
+          ADDITIONAL_LINKS
+          <br>
+        </p>
+      </div>
+        
     </div>
+    <hr>
+  </div>
   `;
+  let additional_links = "";
+  let num_links = all_members.length;
+  for (let i = 0; i < num_links; i++) {
+    additional_links = additional_links.concat(
+      additional_link_template
+        .replace("LINK_TO_REPLACE", pubitem[all_members[i]])
+        .replace("TEXT_TO_REPLACE", all_members[i])
+    );
+  }
   obj.innerHTML = html_template
-  .replace("AUTHORS", pubitem.authors)
-  .replace("TITLE", pubitem.title)
-  .replace("JOURNAL", pubitem.publisher)
-  .replace("IMAGE", pubitem.image)
-  .replace(`<img style="width: 336Px;" alt="Wanli" src="" width="336px" height="200px">`, "");
+    .replace("AUTHORS", pubitem.authors)
+    .replace("TITLE", pubitem.title)
+    .replace("JOURNAL", pubitem.publisher)
+    .replace("IMAGE", pubitem.image)
+    .replace("ADDITIONAL_LINKS", additional_links)
+    .replace(
+      `<img style="width: 336Px;" alt="Wanli" src="" width="336px" height="200px">`,
+      ""
+    );
 
   return obj.content.firstElementChild;
 }
 
+// function journalPubItemConstructor(pubitem) {
+//   let obj;
+//   obj = document.createElement('template');
+//   let html_template = `
+//     <div>
+//       <div class="row">
+//         <div class="small-4 column">
+//             <img src="IMAGE" style="width: 100%">
+//         </div>
+//         <div class="small-8 column">
+//           <p>
+//             <b>TITLE</b>
+//             <br>
+//             AUTHORS
+//             <br> 
+//             JOURNAL
+//             <br>
+//           </p>
+//         </div>
+          
+//       </div>
+//       <hr>
+//     </div>
+//   `;
+//   obj.innerHTML = html_template
+//   .replace("AUTHORS", pubitem.authors)
+//   .replace("TITLE", pubitem.title)
+//   .replace("JOURNAL", pubitem.publisher)
+//   .replace("IMAGE", pubitem.image)
+//   .replace(`<img style="width: 336Px;" alt="Wanli" src="" width="336px" height="200px">`, "");
+
+//   return obj.content.firstElementChild;
+// }
+
 function conferencePubItemConstructor(pubitem) {
-  let obj = document.createElement('template');
+  function filter_mandatory_members(value, index, arr) {
+    let mandatory_members = ["authors", "title", "publisher"];
+    return !mandatory_members.includes(value);
+  }
+  // let all_members = Object.keys(pubitem);
+  // all_members = all_members.filter(filter_mandatory_members)
+  let obj = document.createElement("template");
   let html_template = `
   <div>
+    <h4> CONFERENCE_NAME </h4>
     <div class="row">
       <div class="small-4 column">
         MAIN_IMAGES
@@ -72,31 +134,88 @@ function conferencePubItemConstructor(pubitem) {
     </div>
     <hr>
   </div>
-  `
+  `;
 
   let image_list = "";
   let main_image;
   for (main_image of pubitem.main_images) {
-    image_list += `<img alt="" border="0" src="${main_image}" width="304px" height="200" >`
+    image_list += `<img alt="" border="0" src="${main_image}" width="304px" height="200" >`;
   }
-
 
   let paper_list = "";
   let paper;
+  let all_members;
+  let additional_links;
+  let additional_link_template = `[<a class="aLink" href="LINK_TO_REPLACE" >TEXT_TO_REPLACE</a>]`;
   for (paper of pubitem.papers) {
+    all_members = Object.keys(paper);
+    all_members = all_members.filter(filter_mandatory_members);
+    additional_links = "";
+    let num_links = all_members.length;
+    for (let i = 0; i < num_links; i++) {
+      additional_links = additional_links.concat(
+        additional_link_template
+          .replace("LINK_TO_REPLACE", paper[all_members[i]])
+          .replace("TEXT_TO_REPLACE", all_members[i])
+      );
+    }
     paper_list += `
       <br>
       ${paper.authors}, "${paper.title}", ${paper.publisher}
+      ${additional_links}
       <br>
-    `
+    `;
   }
 
   obj.innerHTML = html_template
-  .replace("MAIN_IMAGES", image_list)
-  .replace("PAPER_LIST", paper_list);
+    .replace("CONFERENCE_NAME", pubitem.conference_name)
+    .replace("MAIN_IMAGES", image_list)
+    .replace("PAPER_LIST", paper_list);
 
   return obj.content.firstElementChild;
 }
+
+// function conferencePubItemConstructor(pubitem) {
+//   let obj = document.createElement('template');
+//   let html_template = `
+//   <div>
+//     <div class="row">
+//       <div class="small-4 column">
+//         MAIN_IMAGES
+//       </div>
+//       <div class="small-8 column">
+//         <p>
+//           PAPER_LIST
+//         </p>
+//       </div>
+//     </div>
+//     <hr>
+//   </div>
+//   `
+
+//   let image_list = "";
+//   let main_image;
+//   for (main_image of pubitem.main_images) {
+//     image_list += `<img alt="" border="0" src="${main_image}" width="304px" height="200" >`
+//   }
+
+
+//   let paper_list = "";
+//   let paper;
+//   for (paper of pubitem.papers) {
+//     paper_list += `
+//       <br>
+//       ${paper.authors}, "${paper.title}", ${paper.publisher}
+//       <br>
+//     `
+//   }
+
+//   obj.innerHTML = html_template
+//   .replace("MAIN_IMAGES", image_list)
+//   .replace("PAPER_LIST", paper_list);
+
+//   return obj.content.firstElementChild;
+// }
 
 //Journal list generation
 let journal_list = document.getElementById("journal_list");
